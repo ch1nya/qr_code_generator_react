feff4588e1703382067bf9ae97a5945a867e9268
ch1nya <you@example.com>
2024-02-09 15:39:47 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import logo from './512x512bb.jpg';
 import './App.css';
+import QRCode from "react-qr-code";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('https://github.com/ch1nya')
+    const [prevValue, setPrevValue] = useState('')
+    const changeHandler = (e)=>{
+        setPrevValue(e.target.value)
+    }
+    const generateQrCode = (e)=>{
+        setValue(prevValue)
+    }
+
+    return (
+        <div className="App">
+            <div className='vignette'>
+                <div className="App-div">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Turn your link into QR-code
+                    </p>
+                    <input
+                        className="App-link"
+                        placeholder='Here'
+                        type='text'
+                        onChange={changeHandler}
+                        value={prevValue}
+                    >
+                    </input>
+
+                    <div>
+                        <button className='buttonSubmit'
+                                type='submit'
+                                onClick={generateQrCode}>Generate
+                        </button>
+                    </div>
+                </div>
+                <div className='output'>
+                    {value && prevValue ? <QRCode className='qrCode' value={value}/> : ''}
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
